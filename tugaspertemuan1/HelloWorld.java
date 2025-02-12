9ff2a22f9ff5280a581d4631ea7e1178c3e754df
ahmadnaufalsidiq-2305732 <naufalsidiq@upi.edu>
2025-02-12 11:10:36 +0700

    Add files via upload

---
+++ tugaspertemuan1/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+    public static void main(String args[]){
+        System.out.println("Hello World, Saya Ahmad Naufal Sidiq NIM 2305732, Saya Senang Belajar Java");
+    }
+}
